b0705a4db35c902e78e0745d030147b1e3d14280
rieznik <vip.alexandrreznik@gmail.com>
2022-08-03 17:04:30 +0300

    complete task three

--- task3.js
+++ task3.js
@@ -1,2 +1,16 @@
 'use strict';
 
+let a = -100;
+let b = 50;
+let c;
+
+if (a >= 0 && b >= 0 ){
+    c = a - b;
+    console.log(c);
+} else if (0 > a && 0 > b) {
+    c = a * b;
+    console.log(c);
+} else {
+    c = a + b;
+    console.log(c);
+}
