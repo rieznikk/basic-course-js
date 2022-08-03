5667fb59b60406392eef38ac143345f301400cca
rieznik <vip.alexandrreznik@gmail.com>
2022-08-03 17:21:17 +0300

    complete task four

--- task4.js
+++ task4.js
@@ -1,2 +1,49 @@
 'use strict';
 
+/**
+ * Функция складывания.
+ * @param {number} plusA - Первое число. 
+ * @param {number} plusB - Второе число.
+ * @returns number
+ */
+function plus (plusA, plusB) {
+    let c = (plusA + plusB);
+    return (c);
+}
+plus(10, 5);
+
+/**
+ * Функция отнимания.
+ * @param {number} minusA - Первое число. 
+ * @param {number} minusB - Второе число.
+ * @returns number
+ */
+function minus (minusA, minusB) {
+    let c = (minusA - minusB);
+    return (c);
+}
+minus(15, 20);
+
+/**
+ * Функция умножения.
+ * @param {number} multiplyA - Первое число. 
+ * @param {number} multiplyB - Второе число.
+ * @returns number
+ */
+function multiply (multiplyA, multiplyB) {
+    let c = (multiplyA * multiplyB);
+    return (c);
+}
+multiply(50, 21);
+
+/**
+ * Функция деления.
+ * @param {number} divideA - Первое число. 
+ * @param {number} divideB - Второе число.
+ * @returns number
+ */
+function divide (divideA, divideB) {
+    let c = (divideA / divideB);
+    return (c);
+}
+divide(5, 0);
