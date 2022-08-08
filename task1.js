32e6eeb7c297c349a1c7dda027325ed935a39722
rieznik <vip.alexandrreznik@gmail.com>
2022-08-08 17:22:03 +0300

    fix task one

--- task1.js
+++ task1.js
@@ -1,30 +1,39 @@
 'use strict'
 
-for (let i = 0 ; i <= 10 ; i++) {
-    // console.log(i);
-    if (i == 0) {
-        console.log(`${i} - это ноль`);
-    } else if (i == 1) {
-        console.log(`${i} - это один`);
-    } else if (i == 2) {
-        console.log(`${i} - это два`);
-    } else if (i == 3) {
-        console.log(`${i} - это три`);
-    } else if (i == 4) {
-        console.log(`${i} - это четыре`);
-    } else if (i == 5) {
-        console.log(`${i} - это пять`);
-    } else if (i == 6) {
-        console.log(`${i} - это шесть`);
-    } else if (i == 7) {
-        console.log(`${i} - это семь`);
-    } else if (i == 8) {
-        console.log(`${i} - это восемь`);
-    } else if (i == 9) {
-        console.log(`${i} - это девять`);
-    } else if (i == 10) {
-        console.log(`${i} - это десять`);
-    } else {
-        break;
+for (let i = 0; i <= 10; i++) {
+    switch (i) {
+        case 0:
+            console.log(`${i} - это ноль`);
+            break;
+        case 1:
+            console.log(`${i} - это один`);
+            break;
+        case 2:
+            console.log(`${i} - это два`);
+            break;
+        case 3:
+            console.log(`${i} - это три`);
+            break;
+        case 4:
+            console.log(`${i} - это четыре`);
+            break;
+        case 5:
+            console.log(`${i} - это пять`);
+            break;
+        case 6:
+            console.log(`${i} - это шесть`);
+            break;
+        case 7:
+            console.log(`${i} - это семь`);
+            break;
+        case 8:
+            console.log(`${i} - это восемь`);
+            break;
+        case 9:
+            console.log(`${i} - это девять`);
+            break;
+        case 10:
+            console.log(`${i} - это десять`);
+            break;
     }
 }
