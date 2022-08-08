15bea1c7d0f06a133b42c4d0413f6f4dfb51c97e
rieznik <vip.alexandrreznik@gmail.com>
2022-08-08 17:30:58 +0300

    still fix task one

--- task1.js
+++ task1.js
@@ -1,39 +1,11 @@
 'use strict'
 
 for (let i = 0; i <= 10; i++) {
-    switch (i) {
-        case 0:
-            console.log(`${i} - это ноль`);
-            break;
-        case 1:
-            console.log(`${i} - это один`);
-            break;
-        case 2:
-            console.log(`${i} - это два`);
-            break;
-        case 3:
-            console.log(`${i} - это три`);
-            break;
-        case 4:
-            console.log(`${i} - это четыре`);
-            break;
-        case 5:
-            console.log(`${i} - это пять`);
-            break;
-        case 6:
-            console.log(`${i} - это шесть`);
-            break;
-        case 7:
-            console.log(`${i} - это семь`);
-            break;
-        case 8:
-            console.log(`${i} - это восемь`);
-            break;
-        case 9:
-            console.log(`${i} - это девять`);
-            break;
-        case 10:
-            console.log(`${i} - это десять`);
-            break;
+    if (i == 0) {
+        console.log(`${i} - это ноль`);
+    } else if (i % 2) {
+        console.log(`${i} - это нечетное число`);
+    } else {
+        console.log(`${i} - это четное число`);
     }
 }
