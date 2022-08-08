b49dfed9e03f2cfa21bdf5aba3137f379e1a2d4b
rieznik <vip.alexandrreznik@gmail.com>
2022-08-08 16:24:55 +0300

    complete task one

---
+++ task1.js
@@ -0,0 +1,30 @@
+'use strict'
+
+for (let i = 0 ; i <= 10 ; i++) {
+    // console.log(i);
+    if (i == 0) {
+        console.log(`${i} - это ноль`);
+    } else if (i == 1) {
+        console.log(`${i} - это один`);
+    } else if (i == 2) {
+        console.log(`${i} - это два`);
+    } else if (i == 3) {
+        console.log(`${i} - это три`);
+    } else if (i == 4) {
+        console.log(`${i} - это четыре`);
+    } else if (i == 5) {
+        console.log(`${i} - это пять`);
+    } else if (i == 6) {
+        console.log(`${i} - это шесть`);
+    } else if (i == 7) {
+        console.log(`${i} - это семь`);
+    } else if (i == 8) {
+        console.log(`${i} - это восемь`);
+    } else if (i == 9) {
+        console.log(`${i} - это девять`);
+    } else if (i == 10) {
+        console.log(`${i} - это десять`);
+    } else {
+        break;
+    }
+}
