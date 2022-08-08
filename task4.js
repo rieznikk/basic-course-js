33c3d2c797e775e4b7b2d75e6e00910caffe240f
rieznik <vip.alexandrreznik@gmail.com>
2022-08-08 15:24:39 +0300

    fix issues

--- task4.js
+++ task4.js
@@ -2,48 +2,44 @@
 
 /**
  * Функция складывания.
- * @param {number} plusA - Первое число. 
- * @param {number} plusB - Второе число.
+ * @param {number} a - Первое число. 
+ * @param {number} b - Второе число.
  * @returns {number}
  */
-function plus (plusA, plusB) {
-    let c = (plusA + plusB);
-    return (c);
+function plus (a, b) {
+    return a + b;
 }
 plus(10, 5);
 
 /**
  * Функция отнимания.
- * @param {number} minusA - Первое число. 
+ * @param {number} a - Первое число. 
  * @param {number} minusB - Второе число.
  * @returns {number}
  */
-function minus (minusA, minusB) {
-    let c = (minusA - minusB);
-    return (c);
+function minus (a, minusB) {
+    return a - b;
 }
 minus(15, 20);
 
 /**
  * Функция умножения.
- * @param {number} multiplyA - Первое число. 
- * @param {number} multiplyB - Второе число.
+ * @param {number} a - Первое число. 
+ * @param {number} b - Второе число.
  * @returns {number}
  */
-function multiply (multiplyA, multiplyB) {
-    let c = (multiplyA * multiplyB);
-    return (c);
+function multiply (a, b) {
+    return a * b;
 }
 multiply(50, 21);
 
 /**
  * Функция деления.
- * @param {number} divideA - Первое число. 
- * @param {number} divideB - Второе число.
+ * @param {number} a - Первое число. 
+ * @param {number} b - Второе число.
  * @returns {number}
  */
-function divide (divideA, divideB) {
-    let c = (divideA / divideB);
-    return (c);
+function divide (a, b) {
+    return a / b;
 }
 divide(5, 0);
