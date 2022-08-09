61e4112a9868c990ed8b1bb014d80eab3456453a
rieznik <vip.alexandrreznik@gmail.com>
2022-08-09 22:17:22 +0300

    Complete task one

--- task1.js
+++ task1.js
@@ -1,2 +1,17 @@
 'use strict'
 
+function checkingNumbers(num) {
+    if (!Number.isInteger(num) || num < 0 || num > 999) {
+        alert(`Значение должно быть от 0 до 999. Спасибо, увидимся.`)
+        return {};
+    }
+    return {
+        units: num % 10,
+        hundreds: Math.floor(num / 100),
+        tens: Math.floor(num / 10) % 10,
+    }
+    
+}
+
+let answer = parseInt(prompt('Введите число от 0 до 999'));
+console.log(checkingNumbers(answer));
