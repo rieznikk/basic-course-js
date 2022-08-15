de60b660d18a23387a533a82083d6d7fdfee13a9
rieznik <vip.alexandrreznik@gmail.com>
2022-08-15 12:53:17 +0300

    Complete task 1-1

--- task1-1.js
+++ task1-1.js
@@ -1,34 +1,103 @@
-'use strict'
+"use strict";
 
-const products = [
-    {
-        id: 3,
-        price: 127,
-        photos: [
-            "1.jpg",
-            "2.jpg",]
-    },
-    {
-        id: 5,
-        price: 499,
-        photos: []
-    },
-    {
-        id: 10,
-        price: 26,
-        photos: [
-            "3.jpg"
-        ]
-    }, {
-        id: 8,
-        price: 78,
-    },
-];
+// function Message(text) {
+//     this.text = text;
+//     this.encrypted = "";
+// }
 
-let productsPhoto = products.filter(products => "photos" in products && products.photos.length > 0);
-console.log (productsPhoto);
-let lowerPriceToHight = products.sort(function(a, b) {
-    return a.price - b.price;
-});
+// Message.prototype.encrypt = function () {
+//     let symbolsAsNumbers = [];
+//     for (let i = 0; i < this.text.length; i++) {
+//         // charCodeAt https://mzl.la/2KExdCr
+//         // push https://mzl.la/3oba48D
+//         symbolsAsNumbers.push(this.text.charCodeAt(i));
+//     }
+//     // join https://mzl.la/3sMknDs
+//     this.encrypted = symbolsAsNumbers.join(" ");
+// };
 
-console.log (lowerPriceToHight);
+// Message.prototype.decrypt = function () {
+//     // split https://mzl.la/3oaa6O2
+//     let encryptedStringAsArray = this.encrypted.split(" ");
+//     // Spread оператор https://mzl.la/3p5PDep
+//     // console.log(...[10, 3, 89]) <--- посмотрите в консоли пример работы spread оператора
+//     // fromCharCode https://mzl.la/3pdsiHH
+//     let decryptedString = String.fromCharCode(...encryptedStringAsArray);
+//     return decryptedString;
+// };
+
+// // let message = new Message("hello world");
+// // message.encrypt();
+// // console.log(message.encrypted);
+// // console.log(message.decrypt());
+
+// function HashMessage(text, hash) {
+//     Message.call(this, text);
+//     this.hash = hash;
+// }
+
+// HashMessage.prototype = Object.create(Message.prototype);
+// HashMessage.prototype.constructor = HashMessage;
+
+// HashMessage.prototype.hashEncrypt = function () {
+//     // btoa https://mzl.la/3c5VTz8
+//     this.encrypted = window.btoa(this.text + "|" + this.hash);
+// };
+
+// HashMessage.prototype.hashDecrypt = function () {
+//     // atob https://mzl.la/3c5VTz8
+//     let fullDecryptedString = window.atob(this.encrypted);
+//     // indexOf https://mzl.la/3c8xCbF
+//     let positionOfSeparator = fullDecryptedString.indexOf("|");
+//     // slice https://mzl.la/365xoyg
+//     return fullDecryptedString.slice(0, positionOfSeparator);
+// }
+
+// let hashMessage1 = new HashMessage("lorem ipsum", "adsf!@#_(*+-&ktyusqewad");
+// hashMessage1.hashEncrypt();
+// console.log(hashMessage1.encrypted);
+// console.log(hashMessage1.hashDecrypt());
+
+// hashMessage1.encrypt();
+// console.log(hashMessage1.encrypted);
+// console.log(hashMessage1.decrypt());
+
+
+/**
+ * ES5
+ * @param {string} name 
+ * @param {number} price 
+ */
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+Product.prototype.make25PercentDiscount = function() {
+    this.price = this.price - (this.price / 100 * 25);
+    return this.price;
+}
+
+// let hookah = new Product('hookah', 5000);
+// console.log(hookah.make25PercentDiscount());
+
+
+/**
+ * ES6
+ * @param {string} name 
+ * @param {number} price 
+ */
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    make25PercentDiscount = function() {
+            this.price = this.price - (this.price / 100 * 25);
+            return this.price;
+        }
+}
+
+// let hookah = new Product('hookah', 5000);
+// console.log(hookah.make25PercentDiscount());
