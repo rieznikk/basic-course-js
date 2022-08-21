ddafe4a4f908291c3548d19cdcaf8264e6e09ea0
rieznik <vip.alexandrreznik@gmail.com>
2022-08-21 13:44:12 +0300

    task2-init

---
+++ task2-config.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Объект с настройками игры.
+ * @property {int} rowsCount Количество строк в карте.
+ * @property {int} colsCount Количество колонок в карте.
+ */
+ const config = {
+    rowsCount: 10,
+    colsCount: 10,
+};
