ddb7bb8d77e76b68404c1a95655df7a5edbe0851
rieznik <vip.alexandrreznik@gmail.com>
2022-09-26 22:34:05 +0300

    task 10

--- 4 уровень/1 вывод товаров/app.js
+++ 4 уровень/1 вывод товаров/app.js
@@ -75,6 +75,13 @@ const products = {
     ],
 };
 
+let productsEl = document.querySelector('.products');
+const btnEl = document.querySelectorAll('button');
+
+btnEl.forEach(function(btnEl){
+    btnEl.addEventListener('click', clickHandler);
+})
+
 
 /**
  * Эта функция должна вызываться при клике по кнопкам.
@@ -82,10 +89,10 @@ const products = {
  */
 function clickHandler(event) {
     //вам нужно очищать содержимое .products
-    
+    productsEl.innerHTML = '';
     //в showCategory надо передать строку с типом категории, тип берите
     //из атрибута data-type у кнопки, по которой кликнули.
-    
+    showCategory(event.target.dataset.type);
 }
 
 /**
@@ -96,7 +103,12 @@ function clickHandler(event) {
  * по которой кликнули.
  */
 function showCategory(category) {
-    
+    const getCategory = products[category];
+    let markupToProductsDiv = '';
+    getCategory.forEach(function (product) {
+        markupToProductsDiv += getProductMarkup(product);
+    });
+    productsEl.insertAdjacentHTML("afterbegin", markupToProductsDiv);
 }
 
 /**
@@ -109,5 +121,12 @@ function showCategory(category) {
  * в верху этого файла.
  */
 function getProductMarkup(product) {
-
+    return `
+    <div class="product">
+        <div>${product.name}</div>
+        <img src="${product.imageUrl}" alt="">
+        <div>${product.price}</div>
+        <a href="https://example.com/producs/${product.id}">Подробнее</a>
+    </div>
+    `
 }
